3b2b5f983aafca440fcba5360eec91211320a252
Mateusz Rojek <mateuszr2047@gmail.com>
2023-11-13 20:13:09 +0100

    Poprawa3

--- src/main/java/pl/javastart/task/PriceCalculator.java
+++ src/main/java/pl/javastart/task/PriceCalculator.java
@@ -2,24 +2,21 @@ package pl.javastart.task;
 
 public class PriceCalculator {
 
-    private static double LOGISTIC_SERVICE_COST = 5;
-    private static double GIFT_TICKET_INCREASE = 0.05;
+    private static final double LOGISTIC_SERVICE_COST = 5;
+    private static final double GIFT_TICKET_INCREASE = 0.05;
 
     public static double calculateFinalPrice(Ticket ticket) {
-        double finalPrice = 0;
 
-        switch (ticket.getType()) {
-            case Ticket.ONLINE_TICKET ->
-                    finalPrice = ticket.getBasicPrice() * (1 - ticket.getDiscount());
-            case Ticket.STANDARD_TICKET ->
-                    finalPrice = ticket.getBasicPrice() * (1 - ticket.getDiscount()) + LOGISTIC_SERVICE_COST;
-            case Ticket.GIFT_TICKET ->
-                    finalPrice = ticket.getBasicPrice() * (1 - ticket.getDiscount()) + LOGISTIC_SERVICE_COST
-                            + GIFT_TICKET_INCREASE * (ticket.getBasicPrice() * (1 - ticket.getDiscount()));
-            default -> finalPrice = 0;
-        }
+        return switch (ticket.getType()) {
+            case Ticket.ONLINE_TICKET -> ticket.getBasicPrice() * (1 - ticket.getDiscount());
 
-        return finalPrice;
+            case Ticket.STANDARD_TICKET -> ticket.getBasicPrice() * (1 - ticket.getDiscount()) + LOGISTIC_SERVICE_COST;
 
+            case Ticket.GIFT_TICKET -> ticket.getBasicPrice() * (1 - ticket.getDiscount()) + LOGISTIC_SERVICE_COST
+                    + GIFT_TICKET_INCREASE * (ticket.getBasicPrice() * (1 - ticket.getDiscount()));
+
+            default -> 0;
+
+        };
     }
 }
